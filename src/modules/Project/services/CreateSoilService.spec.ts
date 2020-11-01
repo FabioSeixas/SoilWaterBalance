da16457634a15e52e9e3869829c963eecd44a3f3
Fabio Seixas <flscosta94@gmail.com>
2020-11-01 06:21:36 -0300

    unit tests for create soil

--- src/modules/Project/services/CreateSoilService.spec.ts
+++ src/modules/Project/services/CreateSoilService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeSoilsRepository from '@modules/Project/repositories/fakes/FakeSoilsRepository';
 import CreateSoilService from '@modules/Project/services/CreateSoilService';
 
@@ -17,5 +19,40 @@ describe('CreateNewSoil', () => {
 
     expect(newSoil).toHaveProperty('id');
     expect(newSoil.author_id).toBe('12312412');
+  });
+
+  it('should not be able to create a new Soil with a name that already exists', async () => {
+    const fakeSoilRepository = new FakeSoilsRepository();
+    const createSoilService = new CreateSoilService(fakeSoilRepository);
+
+    await createSoilService.execute({
+      name: 'NovoSoloTeste',
+      text_class: 'argiloso',
+      total_depth: 200,
+      author_id: '12312412',
+    });
+
+    expect(
+      createSoilService.execute({
+        name: 'NovoSoloTeste',
+        text_class: 'argiloso',
+        total_depth: 200,
+        author_id: '568456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a new Soil with total depth higher than 500 cm', async () => {
+    const fakeSoilRepository = new FakeSoilsRepository();
+    const createSoilService = new CreateSoilService(fakeSoilRepository);
+
+    expect(
+      createSoilService.execute({
+        name: 'NovoSoloTeste',
+        text_class: 'argiloso',
+        total_depth: 600,
+        author_id: '568456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
