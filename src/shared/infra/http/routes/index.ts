76a201f77b9d1615bfbaed42958329a01423c1ee
Fabio Seixas <flscosta94@gmail.com>
2020-11-04 05:45:45 -0300

    user routes little fixes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
 import usersRouter from '@modules/User/infra/http/routes/users.routes';
-import sessionRouter from '@modules/User/infra/http/routes/session.route';
+import sessionRouter from '@modules/User/infra/http/routes/session.routes';
 import soilsRouter from '@modules/Project/infra/http/routes/soils.routes';
+import passwordRouter from '@modules/User/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/session', sessionRouter);
 routes.use('/soils', soilsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,6 +4,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
 import cors from 'cors';
+
+import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from '@shared/infra/http/routes';
 
@@ -13,6 +15,7 @@ import '@shared/container';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
