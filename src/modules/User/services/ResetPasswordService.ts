901750b65bdd6d3bd765afec3d843976da576da1
Fabio Seixas <flscosta94@gmail.com>
2020-11-04 06:26:43 -0300

    little fix on resetPass service

--- src/modules/User/services/ResetPasswordService.ts
+++ src/modules/User/services/ResetPasswordService.ts
@@ -19,7 +19,7 @@ class ResetPasswordService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('UserTokenRepository')
+    @inject('UserTokensRepository')
     private userTokenRepository: IUserTokensRepository,
 
     @inject('HashProvider')
