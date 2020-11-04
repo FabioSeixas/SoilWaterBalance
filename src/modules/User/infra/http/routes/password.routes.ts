5eef52df04563a8a469dfce260685b7808e7df4f
Fabio Seixas <flscosta94@gmail.com>
2020-11-04 05:53:48 -0300

    password controllers

--- src/modules/User/infra/http/routes/password.routes.ts
+++ src/modules/User/infra/http/routes/password.routes.ts
@@ -1,34 +1,15 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
 
-import SendForgotPasswordEmailService from '@modules/User/services/SendForgotPasswordEmailService';
-import ResetPasswordService from '@modules/User/services/ResetPasswordService';
+import ResetPasswordController from '@modules/User/infra/http/controllers/ResetPasswordController';
+import SendForgotPasswordEmailController from '@modules/User/infra/http/controllers/SendForgotPasswordEmailController';
 
 const passwordRouter = Router();
 
-passwordRouter.post('/forgot', async (request, response) => {
-  const { email } = request.body;
+const resetPasswordController = new ResetPasswordController();
+const sendForgotPasswordEmailController = new SendForgotPasswordEmailController();
 
-  const sendForgotPassword = container.resolve(SendForgotPasswordEmailService);
+passwordRouter.post('/forgot', sendForgotPasswordEmailController.create);
 
-  await sendForgotPassword.execute({
-    email,
-  });
-
-  return response.status(204).json();
-});
-
-passwordRouter.post('/reset', async (request, response) => {
-  const { token, password } = request.body;
-
-  const resetPassword = container.resolve(ResetPasswordService);
-
-  await resetPassword.execute({
-    password,
-    token,
-  });
-
-  return response.status(204).json();
-});
+passwordRouter.post('/reset', resetPasswordController.create);
 
 export default passwordRouter;
